d4771c8660875bea70678f8bb45601471d8d2ec1
Tejsvi Tandon <Tejsvi@wizely.in>
2020-04-11 04:27:54 +0530

    Initial project setup

---
+++ SwiftySnackBar/SwiftySnackBar.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySnackBar.h
+//  SwiftySnackBar
+//
+//  Created by Tejsvi Tandon on 11/04/20.
+//  Copyright © 2020 Tejsvi Tandon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftySnackBar.
+FOUNDATION_EXPORT double SwiftySnackBarVersionNumber;
+
+//! Project version string for SwiftySnackBar.
+FOUNDATION_EXPORT const unsigned char SwiftySnackBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySnackBar/PublicHeader.h>
+
+
